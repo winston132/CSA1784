ac0975727a0a6f156a692459778c8e6d9f16a0ae
winston132 <winyarraguntla@gmail.com>
2026-06-15 10:45:37 +0530

    Add files via upload

---
+++ Dieting system prologue.pl
@@ -0,0 +1,6 @@
+% Disease and diet suggestions
+diet(diabetes, 'Low sugar diet').
+diet(bp, 'Low salt diet').
+diet(obesity, 'Low fat diet').
+diet(anemia, 'Iron rich diet').
+diet(ulcer, 'Soft and non-spicy diet').
