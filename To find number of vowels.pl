e80e3fdf7e0f734b9011faaaab5bae200d1152ce
winston132 <winyarraguntla@gmail.com>
2026-06-15 10:49:13 +0530

    Add files via upload

---
+++ To find number of vowels.pl
@@ -0,0 +1,16 @@
+vowel(a).
+vowel(e).
+vowel(i).
+vowel(o).
+vowel(u).
+
+count_vowels([], 0).
+
+count_vowels([H|T], N) :-
+    vowel(H),
+    count_vowels(T, N1),
+    N is N1 + 1.
+
+count_vowels([H|T], N) :-
+    \+ vowel(H),
+    count_vowels(T, N).
